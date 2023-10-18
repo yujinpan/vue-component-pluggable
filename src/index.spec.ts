2c54f33f346bd627ec6687db1d260406ad9169c6
yujinpan <1192878390@qq.com>
2023-10-18 11:58:05 +0800

    feat: add `defineComponent` interface for pluggable component

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,38 @@
-import { helloWorld } from './index';
+import Vue from 'vue';
 
-describe('helloWorld', () => {
-  it('should say', function () {
-    expect(helloWorld({ msg: 'Hello, World!' })).toBe('Hello, World!');
+import { defineComponent } from './index';
+
+describe('defineComponent', () => {
+  it('should define pluggable component', function () {
+    const pluggableComponent = defineComponent({
+      name: 'Pluggable',
+      props: {
+        test: String,
+      },
+      setup(props) {
+        return () => props.test;
+      },
+    });
+    Vue.use(pluggableComponent);
+
+    expect(Vue.component(pluggableComponent.name)).toBeDefined();
+  });
+
+  it('should define pluggable component with custom install', function () {
+    const config: { test?: number } = {};
+    const pluggableComponent = defineComponent({
+      name: 'Pluggable',
+      props: {},
+      install(vue, options: typeof config) {
+        vue.component(this.name, this);
+        Object.assign(config, options);
+      },
+    });
+    Vue.use(pluggableComponent, {
+      test: 1,
+    });
+
+    expect(Vue.component(pluggableComponent.name)).toBeDefined();
+    expect(config.test).toBe(1);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,166 @@
-export function helloWorld(options: { msg: string }) {
-  // eslint-disable-next-line no-console
-  console.log(options.msg);
-  return options.msg;
+import type { PluginObject, VueConstructor } from 'vue';
+import type { HasDefined } from 'vue/types/common';
+import type { CreateElement, RenderContext } from 'vue/types/umd';
+import type {
+  ComponentOptionsWithProps,
+  ComponentOptionsWithArrayProps,
+  ComponentOptionsMixin,
+  ComputedOptions,
+  MethodOptions,
+  ComponentOptionsWithoutProps,
+} from 'vue/types/v3-component-options';
+import type {
+  ExtractPropTypes,
+  ComponentPropsOptions,
+} from 'vue/types/v3-component-props';
+import type { DefineComponent } from 'vue/types/v3-define-component';
+import type { EmitsOptions } from 'vue/types/v3-setup-context';
+
+// fork from v3-define-component.d.ts
+
+export function defineComponent<
+  PluginOptions,
+  RawBindings,
+  D = {},
+  C extends ComputedOptions = {},
+  M extends MethodOptions = {},
+  Mixin extends ComponentOptionsMixin = ComponentOptionsMixin,
+  Extends extends ComponentOptionsMixin = ComponentOptionsMixin,
+  Emits extends EmitsOptions = {},
+  EmitsNames extends string = string,
+>(
+  options: { functional?: never } & ComponentOptionsWithoutProps<
+    {},
+    RawBindings,
+    D,
+    C,
+    M,
+    Mixin,
+    Extends,
+    Emits,
+    EmitsNames
+  > &
+    Partial<PluginObject<PluginOptions>>,
+): DefineComponent<{}, RawBindings, D, C, M, Mixin, Extends, Emits> &
+  PluginObject<PluginOptions>;
+
+export function defineComponent<
+  PluginOptions,
+  PropNames extends string,
+  RawBindings = {},
+  D = {},
+  C extends ComputedOptions = {},
+  M extends MethodOptions = {},
+  Mixin extends ComponentOptionsMixin = ComponentOptionsMixin,
+  Extends extends ComponentOptionsMixin = ComponentOptionsMixin,
+  Emits extends EmitsOptions = {},
+  EmitsNames extends string = string,
+  PropsOptions extends ComponentPropsOptions = ComponentPropsOptions,
+>(
+  options: { functional?: never } & ComponentOptionsWithArrayProps<
+    PropNames,
+    RawBindings,
+    D,
+    C,
+    M,
+    Mixin,
+    Extends,
+    Emits,
+    EmitsNames
+  > &
+    Partial<PluginObject<PluginOptions>>,
+): DefineComponent<
+  Readonly<{ [key in PropNames]?: any }>,
+  RawBindings,
+  D,
+  C,
+  M,
+  Mixin,
+  Extends,
+  Emits
+> &
+  PluginObject<PluginOptions>;
+
+export function defineComponent<
+  PluginOptions,
+  Props,
+  RawBindings = {},
+  D = {},
+  C extends ComputedOptions = {},
+  M extends MethodOptions = {},
+  Mixin extends ComponentOptionsMixin = ComponentOptionsMixin,
+  Extends extends ComponentOptionsMixin = ComponentOptionsMixin,
+  Emits extends EmitsOptions = {},
+  EmitsNames extends string = string,
+  PropsOptions extends ComponentPropsOptions = ComponentPropsOptions,
+>(
+  options: (HasDefined<Props> extends true
+    ? { functional?: never } & ComponentOptionsWithProps<
+        PropsOptions,
+        RawBindings,
+        D,
+        C,
+        M,
+        Mixin,
+        Extends,
+        Emits,
+        EmitsNames,
+        Props
+      >
+    : { functional?: never } & ComponentOptionsWithProps<
+        PropsOptions,
+        RawBindings,
+        D,
+        C,
+        M,
+        Mixin,
+        Extends,
+        Emits,
+        EmitsNames
+      >) &
+    Partial<PluginObject<PluginOptions>>,
+): DefineComponent<PropsOptions, RawBindings, D, C, M, Mixin, Extends, Emits> &
+  PluginObject<PluginOptions>;
+
+export function defineComponent<
+  PluginOptions,
+  PropNames extends string,
+  Props = Readonly<{ [key in PropNames]?: any }>,
+>(
+  options: {
+    functional: true;
+    props?: PropNames[];
+    render?: (h: CreateElement, context: RenderContext<Props>) => any;
+  } & Partial<PluginObject<PluginOptions>>,
+): DefineComponent<Props> & PluginObject<PluginOptions>;
+
+export function defineComponent<
+  PluginOptions,
+  PropsOptions extends ComponentPropsOptions = ComponentPropsOptions,
+  Props = ExtractPropTypes<PropsOptions>,
+>(
+  options: {
+    functional: true;
+    props?: PropsOptions;
+    render?: (h: CreateElement, context: RenderContext<Props>) => any;
+  } & Partial<PluginObject<PluginOptions>>,
+): DefineComponent<PropsOptions> & PluginObject<PluginOptions>;
+
+export function defineComponent(options: any) {
+  if (!options.install) {
+    if (!options.name) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        '[vue-component-pluggable]',
+        'defineComponent required option `name`.',
+      );
+    } else {
+      options.install = (vue: VueConstructor) =>
+        vue.component(options.name, options);
+    }
+  } else {
+    options.install = options.install.bind(options);
+  }
+
+  return options;
 }
